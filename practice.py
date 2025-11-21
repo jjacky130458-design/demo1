b9c4093ff0a09e89154338c73bb000d70e737ff1
jjacky130458-design <jjacky130458@gmail.com>
2025-11-21 09:18:03 +0800

    Fix formatting of objective message in practice.py

--- practice.py
+++ practice.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Business Dashboard with Streamlit Layouts")
 
-msg  = "##Objective: To demonstrate the usage of columns, tabs, and dynamic containers in a business dashboard."
+msg  = "## Objective: To demonstrate the usage of columns, tabs, and dynamic containers in a business dashboard."
 st.write(msg)
